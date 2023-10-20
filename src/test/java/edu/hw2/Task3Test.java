62c26bb5ce92e5b0128d2600941fd2aa0e568c33
belik13 <bmaks62@mail.ru>
2023-10-20 19:31:39 +0300

    Updated Test

--- src/test/java/edu/hw2/Task3Test.java
+++ src/test/java/edu/hw2/Task3Test.java
@@ -1,12 +1,16 @@
 package edu.hw2;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import java.util.Random;
 import edu.hw2.Task3.Connection;
+import edu.hw2.Task3.ConnectionException;
 import edu.hw2.Task3.ConnectionManager;
 import edu.hw2.Task3.DefaultConnectionManager;
 import edu.hw2.Task3.FaultyConnection;
 import edu.hw2.Task3.FaultyConnectionManager;
+import edu.hw2.Task3.PopularCommandExecutor;
 import edu.hw2.Task3.StableConnection;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -37,5 +41,24 @@ public class Task3Test {
         ConnectionManager connectionManager = new DefaultConnectionManager(random);
         Connection actual = connectionManager.getConnection();
         assertThat(actual).isInstanceOf(FaultyConnection.class);
+    }
+    @Test
+    @DisplayName("tryExecute test with Exception")
+    public void wException() {
+        PopularCommandExecutor executor = new PopularCommandExecutor(
+            new DefaultConnectionManager(new Random(78)),
+            1
+        );
+        assertThatThrownBy(executor::updatePackages).isInstanceOf(ConnectionException.class);
+    }
+
+    @Test
+    @DisplayName("tryExecute test w/o Exception")
+    public void noException() {
+        PopularCommandExecutor executor = new PopularCommandExecutor(
+            new DefaultConnectionManager(new Random(2)),
+            2
+        );
+        assertThatCode(executor::updatePackages).doesNotThrowAnyException();
     }
 }
